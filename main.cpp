7288c31b1e20e1c136afb71ec09776818b87722b
Jayanta Roy <93045961+jayanta8509@users.noreply.github.com>
2023-03-08 15:09:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Write a program to reverse a given number
+#include <iostream>
+using namespace std;
+void reverse_1(int ok) {
+  int reverse = 0;
+  while (ok != 0) {
+    reverse = reverse * 10 + ok % 10;
+    ok /= 10;
+  }
+  cout << reverse << endl;
+}
+
+int main() {
+  int a = 675;
+  reverse_1(a);
+}
